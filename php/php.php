c7060170335a1f9462fb61f82a99e7b46614471d
DeltaStorm Studios <50902386+deltastormstudios@users.noreply.github.com>
2019-05-31 00:32:55 +0200

    Update php.php

--- php/php.php
+++ php/php.php
@@ -1,26 +1,16 @@
 echo "NONONONO";
 <?php
+
+if  (isset($_POST['submit'])){
     $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
-   
-
-    $email_from = 'deltastormstudios@gmail.com';
-      
-    $email_subject = "New Form Submission";
-
-    $email_body = "User Name: $name. \n".
-                    "User Email:  $visitor_email.\n".
-                       "User Message: $message. \n";
     
-
-    $to = "deltastormstudios@gmail.com";
-
-    $headers = "From: $email_from \r\n";  
-
-    $headers .= "Reply-To: $visitor_email \r\n";
- 
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: about.html");
-?>
+    $mailTo = "deltastormstudios@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
