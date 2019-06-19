fc879c712fd0c7fd55f000079213dd72ae6d646f
REZDEREZ <aqieloostenbrug@hotmail.nl>
2019-06-19 22:18:34 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -7,11 +7,14 @@ function countdown(){
     if(rdays>=1){
     document.getElementById("days").innerHTML=days;
     }
-    else if(rdays>=0){
+    else if(rdays>=-0.5){
     document.getElementById("releaseTime").innerHTML="A new video will get released within a day.";
     }
-    else if(rdays<0){
-    document.getElementById("releaseTime").innerHTML="The new videos' release date isn't yet determined.";
+    else if(days<1){
+    document.getElementById("releaseTime").innerHTML="A new video has been released.";
     }
+    else if(days<1){
+    document.getElementById("releaseTime").innerHTML="The new videos' release date hasn't yet been determined.";
+    } 
   }
   window.onload = countdown;
