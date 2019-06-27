d96dde8d07353520859447ca934d9a5687dd8077
DeltaStorm Studios <50902386+deltastormstudios@users.noreply.github.com>
2019-06-27 20:37:35 +0200

    Update php.php

--- php/php.php
+++ php/php.php
@@ -1,16 +1,1 @@
-echo "NONONONO";
-<?php
 
-if  (isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
-    
-    $mailTo = "326006@mariscollege.nl";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
-    
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
-}
