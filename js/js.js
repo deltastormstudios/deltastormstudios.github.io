e9f1a0be6e604413765d7985aff7d6f3e9ce4413
DeltaStorm Studios <50902386+deltastormstudios@users.noreply.github.com>
2019-06-27 20:37:04 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,20 +1,17 @@
-function countdown(){
-    var setTime= new Date("June 29, 2019 00:00:00");
-    var Time= new Date();
-    var leftTime= setTime.getTime()-Time.getTime();
-    var days = Math.floor(leftTime/1000/60/60/24);
-    var rdays= leftTime/1000/60/60/24;
-    if(rdays>=1){
-    document.getElementById("releaseTime").innerHTML=days + " day(s) left until a new video releases.";
+function countdown() {
+    var setTime = new Date("June 29, 2019 00:00:00");
+    var Time = new Date();
+    var leftTime = setTime.getTime() - Time.getTime();
+    var days = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+    var rdays = leftTime / 1000 / 60 / 60 / 24;
+    if (rdays >= 1) {
+        document.getElementById("releaseTime").innerHTML = days + " day(s) left until a new video releases.";
+    } else if (rdays >= -0.5) {
+        document.getElementById("releaseTime").innerHTML = "A new video will get released within a day.";
+    } else if (days < 1) {
+        document.getElementById("releaseTime").innerHTML = "A new video has been released.";
+    } else if (days < 2) {
+        document.getElementById("releaseTime").innerHTML = "The new videos' release date hasn't yet been determined.";
     }
-    else if(rdays>=-0.5){
-    document.getElementById("releaseTime").innerHTML="A new video will get released within a day.";
-    }
-    else if(days<1){
-    document.getElementById("releaseTime").innerHTML="A new video has been released.";
-    }
-    else if(days<2){
-    document.getElementById("releaseTime").innerHTML="The new videos' release date hasn't yet been determined.";
-    } 
-  }
-  window.onload = countdown;
+}
+window.onload = countdown;
