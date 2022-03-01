c870e22404e5326ae5172cc83c4b483eb8a96ff5
DeltaStorm Studios <50902386+deltastormstudios@users.noreply.github.com>
2022-03-01 19:03:50 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,17 +1,34 @@
+
 function countdown() {
-    var setTime = new Date("June 29, 2019 00:00:00");
+
+    var setTime = new Date("November 21, 2019 00:00:00");
+
     var Time = new Date();
+
     var leftTime = setTime.getTime() - Time.getTime();
+
     var days = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+
     var rdays = leftTime / 1000 / 60 / 60 / 24;
+
     if (rdays >= 1) {
+
         document.getElementById("releaseTime").innerHTML = days + " day(s) left until a new video releases.";
+
     } else if (rdays >= -0.5) {
+
         document.getElementById("releaseTime").innerHTML = "A new video will get released within a day.";
+
     } else if (days < -0.5 && days >= -1) {
+
         document.getElementById("releaseTime").innerHTML = "A new video has been released.";
+
     } else if (days <-1) {
+
         document.getElementById("releaseTime").innerHTML = "The new videos' release date hasn't yet been determined.";
+
     }
+
 }
+
 window.onload = countdown;
